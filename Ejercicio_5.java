d1ca968de23a4786d800a760be88b01d62a2b6f3
Juanqu261 <125746529+Juanqu261@users.noreply.github.com>
2023-02-25 10:47:48 -0500

    Ejercicio 5, con lectura por teclado

--- Ejercicio_5.java
+++ Ejercicio_5.java
@@ -1,7 +1,14 @@
 import java.lang.Math;
+import java.util.Scanner;
+
 public class Ejercicio_5 {
     public static void main(String[] args) {
-        double suma = 0;
+        double suma;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el numero a operar");
+        suma = sc.nextDouble();
+        sc.close();
+        
         double x = 20;
         suma = suma + x;
         double y = 40;
