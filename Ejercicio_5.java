c7011ae48011e5201957c122c2be59be1c0e42d8
Juanqu261 <125746529+Juanqu261@users.noreply.github.com>
2023-02-25 10:57:17 -0500

    Ejercicio SUMA, operar dos numeros

--- Ejercicio_5.java
+++ Ejercicio_5.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class Ejercicio_5 {
     public static void main(String[] args) {
-        double suma;
+        double suma = 0 ;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el numero a operar");
-        suma = sc.nextDouble();
-        sc.close();
 
-        double x = 20;
+        double x;
+        System.out.println("Ingrese el numero a operar");
+        x = sc.nextDouble();
         suma = suma + x;
-        double y = 40;
+
+        double y;
+        System.out.println("Ingrese el segundo numero a operar");
+        y = sc.nextDouble();
+        sc.close();
         x = x + Math.pow(y, 2);
         suma = suma + x/y;
 
